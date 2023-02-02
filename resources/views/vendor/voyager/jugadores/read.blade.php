f6ce3969e8614a95c7058af0f0150da2bf37ed88
jona1102cm <j.emanuel.1102@gmail.com>
2023-02-02 11:11:02 -0400

    update read jugador

--- resources/views/vendor/voyager/jugadores/read.blade.php
+++ resources/views/vendor/voyager/jugadores/read.blade.php
@@ -2,6 +2,11 @@
 
 @section('page_title', __('voyager::generic.view').' '.$dataType->getTranslatedAttribute('display_name_singular'))
 
+@section('css')
+<link href="{{setting('admin.url')}}plugin_profile/css/tm_editable.css" type="text/css"  rel="stylesheet">
+<link href="{{setting('admin.url')}}plugin_profile/css/tm_validator.css" type="text/css"  rel="stylesheet">
+<link href="{{setting('admin.url')}}plugin_profile/css/themes/round-button-theme.css" type="text/css"  rel="stylesheet" >
+@stop
 
 @section('content')
     @php
@@ -9,6 +14,7 @@
         $jugadore=App\Jugadore::where('id', $dataTypeContent->id)->with('clubes', 'transferencias')->first();
         $rel_temporada= App\RelTemporadaJugadore::where('jugadore_id', $dataTypeContent->id)->with('temporadas', 'clubes')->get();
         $transferencias= App\Transferencia::where('jugadore_id', $dataTypeContent->id)->with('club_origen', 'club_destino')->get();
+        $equipos=App\Clube::all();
     @endphp
     <div class="page-content read container-fluid">
         <div class="row">
@@ -39,33 +45,85 @@
                                         </tr>
                                         <tr>
                                             <td><b>Nombre:</b></td>
-                                            <td>{{$dataTypeContent->name}}</td>
+                                            <td >
+                                                <div class="tm_editable_container input-group theme1 " id="text_demo_nombre" data-iplaceholder="Escribe el Nombre..">
+                                                    <input id="input_name" type="text" value ="{{$dataTypeContent->name}}" >
+                                                </div>
+                                            </td>
+
+                                          
                                         </tr>
                                         <tr>
                                             <td><b>Nº Polera:</b></td>
-                                            <td>{{$dataTypeContent->polera}}</td>
+                                            <td >
+                                                <div class="tm_editable_container input-group theme1" id="text_demo_polera" data-iplaceholder="Escribe el Nombre..">
+                                                    <input type="text" value ="{{$dataTypeContent->polera}}" >
+                                                </div>
+                                            </td>
                                         </tr>
                                         <tr>
                                             <td><b>Edad:</b></td>
-                                            <td>{{$dataTypeContent->edad}}</td>
+                                            <td >
+                                                <div class="tm_editable_container input-group theme1" id="text_demo_edad" data-iplaceholder="Escribe el Nombre..">
+                                                    <input type="text" value ="{{$dataTypeContent->edad}}" >
+                                                </div>
+                                            </td>
                                         </tr>
                                             
                                         <tr>
                                             <td><b>Fecha Nacimiento:</b></td>
-                                            <td colspan="2">{{$dataTypeContent->nacimiento}}</td>
+                                            <td colspan="2">
+                                                {{-- {{$dataTypeContent->nacimiento}} --}}
+                                                <div class="tm_editable_container input-group theme1" id="text_demo_nacimiento" >
+                                                    <input type="text" value="{{$dataTypeContent->nacido}}">
+                                                </div>
+                                                
+                                            </td>
                                         </tr>
                                         <tr>
                                             <td><b>Categoria:</b></td>
-                                            <td colspan="2">{{$dataTypeContent->jug_categoria}}</td>
+                                            <td colspan="2">
+                                                <div class="tm_editable_container input-group " id="select_demo_categoria">
+                                                    <select class=" my_select">
+                                                        @if ($dataTypeContent->jug_categoria=="Senior")
+                                                            <option value="Senior" selected="selected">Senior</option>
+                                                            <option value="Especial" >Especial</option>
+                                                        @else
+                                                            <option value="Senior">Senior</option>
+                                                            <option value="Especial" selected="selected">Especial</option>
+                                                        @endif
+                                                      
+                                                    </select>
+                                                </div>
+                                               
+                                            </td>
                                         </tr>
                                         <tr>
                                             <td><b>Club Actual:</b></td>
-                                            <td colspan="2">{{$jugadore->clubes->name}}</td>
+                                            {{-- <td colspan="2">{{$jugadore->clubes->name}}</td> --}}
+                                            <td colspan="2">
+                                                <div class="tm_editable_container input-group " id="select_demo_club">
+                                                    <select class=" my_select">
+                                                        @foreach ($equipos as $item)
+                                                            @if($item->id == $jugadore->clubes->id)
+                                                                <option value="{{$item->id}}" selected>{{$item->name}}</option>
+                                                            @else
+                                                                <option value="{{$item->id}}" >{{$item->name}}</option>
+                                                            @endif
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </td>
                                         </tr>
                                   
                                         <tr>
                                             <td><b>WhatsApp:</b></td>
-                                            <td colspan="2">{{$dataTypeContent->phone}}</td>
+                                            <td colspan="2" >
+                                                <div class="tm_editable_container input-group theme1 " id="text_demo_whatsapp" data-iplaceholder="Escribe el WhatsApp..">
+                                                    <input type="text" value ="{{$dataTypeContent->phone}}" >
+
+                                                </div>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -298,6 +356,9 @@
         });
 
     </script> --}}
+    {{-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>  --}}
+    <script src="{{setting('admin.url')}}plugin_profile/js/tm_validator.js"></script> 
+    <script src="{{setting('admin.url')}}plugin_profile/js/tm_editable.js"></script> 
     <script>
         async function send_carnet() {
             if (await validacion_wpp("{{$dataTypeContent->phone}}")) {
@@ -347,13 +408,132 @@
             }
         }
         $('document').ready(function () {
-        
+            $('#text_demo_nombre, .width_auto_demo').tm_editbale('init', initial('text_demo_nombre', 'name', 'tm-edit-0'))
+            $('#text_demo_polera, .width_auto_demo').tm_editbale('init', initial('text_demo_polera', 'polera', 'tm-edit-1'))
+            $('#text_demo_edad, .width_auto_demo').tm_editbale('init', initial('text_demo_edad', 'edad', 'tm-edit-2'))
+            $('#text_demo_nacimiento, .width_auto_demo').tm_editbale('init', initial('text_demo_nacimiento', 'nacido', 'tm-edit-3'))
+            $('#select_demo_categoria, .width_auto_demo').tm_editbale('init', initial('select_demo_categoria', 'jug_categoria', 'tm-edit-4'))
+            $('#select_demo_club, .width_auto_demo').tm_editbale('init', initial('select_demo_club', 'clube_id', 'tm-edit-5'))
+            $('#text_demo_whatsapp, .width_auto_demo').tm_editbale('init', initial('text_demo_whatsapp', 'phone', 'tm-edit-6'))
         });
         async function send_carnet_jugador() {
             var phone= "{{$dataTypeContent->phone}}"
             if (phone!="") {
                 
             }
+        }
+        function initial(text_demo, name, div_id) {
+          
+            var array= {
+                            theme:'round-button-theme',
+                            outside_btn:{
+                                            onshow:"&nbsp;<i class='voyager-edit'></i>&nbsp;",
+                                            new_line:false,
+                                            onhover:''
+                                        },
+                            inside_btn:{
+                                            
+                                            new_line:false,
+                                            ok:"<i class='fa fa-check'></i>",
+                                            cancel:"<i class='fa fa-times'></i>"
+                                        },
+                            ok:{
+                                before:function(value){
+                                    var deferred = $.Deferred();
+                                    //your code start here
+                                
+                                    //alert(value) //value of element
+                                    // $("#tm-edit-0 .i_text").html("hola")
+                                    validar_input(text_demo, value, name, div_id)
+                                    //your code ends here
+                                    deferred.resolve();
+                                    return deferred.promise();
+                                },
+                                after:function(){
+                                    //$("#tm-edit-0 .i_text").html("{{$dataTypeContent->name}}")
+                                    // $("#tm-edit-0").html("<div class='tm_editable_container input-group theme1 has_tm_editable_container round-button-theme'><div class='no_edit full_row'><span class='i_text'>{{$dataTypeContent->name}}</span><span class='outside_btncontainer right'><a class='i_edit'><i class='voyager-edit'></i><span class='i_edit_text'></span></a></span></div></div>")
+                                }
+                            } ,
+                            remove:{
+                                before:function(){
+                                    var deferred = $.Deferred();
+                                    //your code start here
+                                    alert('hello')
+                                    //your code ends here
+                                    deferred.resolve();
+                                    return deferred.promise();
+                                },
+                                after:function(){}
+                            }
+                        
+                        }
+
+            return array;
+            
+        }
+        async function validar_input(text_demo, value, name, div_id) {
+            if(value!=""){
+                switch (name) {
+                    case 'polera':
+                            if (parseInt(value)>0) {
+                                var midata={id: "{{$dataTypeContent->id}}",valor: value, name: name}
+                                await axios.post("/api/jugadores/update/jugador", midata)
+                                toastr.success("Actualizó el número de polera exitosamente")
+                            }
+                            else{
+                                toastr.error("Ingrese un numero de polera válido")
+                            }
+                        
+                        
+                      
+                        break;
+                    case 'edad':
+                        if (parseInt(value)>0) {
+                            var midata={id: "{{$dataTypeContent->id}}",valor: value, name: name}
+                            await axios.post("/api/jugadores/update/jugador", midata)
+                            toastr.success("Actualizó la edad exitosamente")
+                        }
+                        else{
+                            toastr.error("Ingrese una edad válida")
+                        }
+                        break;
+                    case 'nacido':
+                        let isValidDate = Date.parse(value);
+
+                        if (isNaN(isValidDate)) {
+                            var midata={id: "{{$dataTypeContent->id}}",valor: value, name: name}
+                            await axios.post("/api/jugadores/update/jugador", midata)
+                            toastr.success("Actualizó la fecha de nacimiento exitosamente")
+                        }
+                        else{
+                            toastr.error("Ingrese una fecha válida")
+                        }
+                      
+                        break;
+                    case 'phone':
+                        if (parseInt(value)>59999999 && parseInt(value)<80000000) {
+                            var midata={id: "{{$dataTypeContent->id}}",valor: value, name: name}
+                            await axios.post("/api/jugadores/update/jugador", midata)
+                            toastr.success("Actualizó el número de celular exitosamente")
+                        }
+                        else{
+                            toastr.error("Ingrese un numero de celular válido")
+                        }
+                        break;
+                    default:
+                    console.log("guardar")
+                    var midata={id: "{{$dataTypeContent->id}}",valor: value, name: name}
+                            await axios.post("/api/jugadores/update/jugador", midata)
+                            toastr.success("Actualizó el campo exitosamente")
+                            break;
+                }
+               
+            }
+            else{
+                toastr.error("No puede Guardar el campo vacío")
+                location.reload()
+            }
+
         }
     </script>
 @stop
